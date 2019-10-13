88bb22141de1efeabf2c3b65388d699e90eea584
Gilles Fabre <gillesfabrejob@gmail.com>
2019-10-13 15:47:59 +0200

    TAG genese-angular v0.0.9

--- src/app/demo/home/home.module.ts
+++ src/app/demo/home/home.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from '../../app-routing.module';
 import { CoreModule } from '../core/core.module';
 import { DescriptionComponent } from './description/description.component';
 import { MethodService } from './services/method.service';
-import { GeneseModule } from 'genese-angular';
 
 
 @NgModule({
