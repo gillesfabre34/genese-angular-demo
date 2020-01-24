b7e66fb913176d5ce6e5119b77f5f55a60a42aac
Gilles FABRE <gillesfabrejob@gmail.com>
2020-01-24 17:43:06 +0100

    Adding create's form (#26)
    * Added create book form
    
    * Added enum for swagger

--- backend/src/books/dto/create-book.dto.ts
+++ backend/src/books/dto/create-book.dto.ts
@@ -1,18 +1,25 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Category } from '../enums/category';
 
 export class CreateBookDTO {
-    @ApiProperty({
-        description: 'The id of the book',
-        default: 1,
-    })
-    readonly id: number;
     @ApiProperty({
         description: 'The title of the book',
         minLength: 1
     })
     readonly title: string;
+
+    @ApiProperty({
+        description: 'The author of the book',
+        default: 1,
+    })
+    readonly author: string;
+
     @ApiPropertyOptional()
     readonly description: string;
-    @ApiProperty()
-    readonly author: string;
+
+    @ApiPropertyOptional({
+        description: 'The book\'s category',
+        default: '',
+    })
+    readonly category: Category;
 }
